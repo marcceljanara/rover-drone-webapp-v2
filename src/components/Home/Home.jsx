92dc3050c9be0ea7e5484b680197d45993af3902
salman <salmanabdurrohman21@gmail.com>
2025-07-01 15:11:56 +0700

    perbaikan fitur scroll manual di landing pages

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 import Navbar from '../Navbar/Navbar';
 import Intro from '../Intro/Intro';
 import Services from '../Services/Services';
 import Works from '../Works/Works';
 import Footer from '../Footer/Footer';
+import { Element, scroller } from 'react-scroll';
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location?.state?.scrollTo) {
+      scroller.scrollTo(location.state.scrollTo, {
+        duration: 500,
+        smooth: true,
+        offset: -70,
+      });
+    }
+  }, [location]);
+
   return (
     <div className="home-wrapper">
-      <div className="home">
-        <div id="Navbar">
-          <Navbar />
-        </div>
-        <div id="intro">
+      <Navbar />
+      <div className="home-content">
+        <Element name="intro">
           <Intro />
-        </div>
-        <div id="services">
+        </Element>
+
+        <Element name="services">
           <Services />
-        </div>
-        <div id="works">
+        </Element>
+
+        <Element name="works">
           <Works />
-        </div>
+        </Element>
+
         <Footer />
       </div>
     </div>
