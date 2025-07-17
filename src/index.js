65019972d4c5f87fc6ca96cacab3a2c2e9d6db5b
salman <salmanabdurrohman21@gmail.com>
2025-07-18 02:56:59 +0700

    Memperbaiki sistem PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ⬅️ Tambahkan ini
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -11,3 +12,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// ⬇️ Tambahkan ini untuk mengaktifkan PWA
+serviceWorkerRegistration.register();
