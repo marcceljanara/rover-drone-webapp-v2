44e394d3380b76966bc5a5b0aa7576e1a31a88c3
Marccel Janara <nengahyuichiro34@gmail.com>
2025-08-01 11:33:19 +0700

    Refactor CompactCard: uncomment radialBar div and comment out CircularProgressbar import

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -33,10 +33,10 @@ function CompactCard({ param, setExpanded }) {
       layoutId="expandableCard"
       onClick={setExpanded}
     >
-      {/* <div className="radialBar">
-        <CircularProgressbar value={param.barValue} text={`${param.barValue}%`} />
+      <div className="radialBar">
+        {/* <CircularProgressbar value={param.barValue} text={`${param.barValue}%`} /> */}
         <span>{param.title}</span>
-      </div> */}
+      </div>
       <div className="detail">
         <Png />
         <span>{param.value}{param.satuan}</span>
